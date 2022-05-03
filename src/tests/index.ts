faa12a136a4be777bdfb67f96fef45f65d9c57b3
AircraftOverviewer <maltesers42@gmail.com>
2022-05-04 09:48:53 +1000

    refector: update test

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,5 +1,12 @@
+import { Cities } from '../client/structures';
 import { BOM } from '../index';
 
 (async () => {
-	console.log(await BOM.getWarnings('r68mtc'));
+	const observations = await BOM.getObservations(Cities.ADELAIDE);
+	const warnings = await BOM.getWarnings(Cities.BRISBANE);
+	const dailyForecast = await BOM.getDailyForecast(Cities.CANBERRA);
+	const threeHourForecast = await BOM.getThreeHourForecast(Cities.DARWIN);
+	const rainForecast = await BOM.getRainForecast(Cities.HOBART);
+
+	console.log(observations, warnings, dailyForecast, threeHourForecast, rainForecast);
 })().catch(console.error);
